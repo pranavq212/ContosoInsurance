e2c50922c49abe56b4337ebc42e7eb3818a1d01c
Cory Fowler <cfowler@microsoft.com>
2016-12-07 14:26:19 -0800

    removing bootstrap as a nuget asset

--- Src/Cloud/ContosoInsurance.MVC/App_Start/BundleConfig.cs
+++ Src/Cloud/ContosoInsurance.MVC/App_Start/BundleConfig.cs
@@ -20,7 +20,6 @@ namespace ContosoInsurance.MVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
