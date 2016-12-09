73f36c75644dd5831535fc280f815e72a770a781
Cory Fowler <cfowler@microsoft.com>
2016-12-09 15:40:57 -0800

    commenting out bootstrap.js

--- Src/Cloud/ContosoInsurance.MVC/App_Start/BundleConfig.cs
+++ Src/Cloud/ContosoInsurance.MVC/App_Start/BundleConfig.cs
@@ -20,6 +20,7 @@ namespace ContosoInsurance.MVC
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      //"~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
